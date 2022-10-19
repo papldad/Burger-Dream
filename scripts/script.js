89a768324a3949d66f738e0f8cf74078ebd30069
Pavel <papldad@mail.ru>
2022-10-19 16:02:40 +0300

    fix: fixed position btn menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 function onClickMenu() {
     document.getElementById("header").classList.toggle("open-header");
-    document.getElementById("menu-lines").classList.toggle("open-menu");
+    document.getElementById("menu").classList.toggle("open-menu");
     document.getElementById("nav").classList.toggle("open-nav");
 }
